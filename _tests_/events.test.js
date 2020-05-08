508ed2d7abf71a5e0d65ddbad5c74494e4fcd325
Randee Orion <randeegood@gmail.com>
2020-05-07 18:38:13 -0700

    stuff

--- _tests_/events.test.js
+++ _tests_/events.test.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const globalEmitter = require('../lib/events');
+const pickupHandler = require('../lib/pickupHandler');
+const inTransitHandler = require('../lib/inTransitHandler');
+const deliveredHandler = require('../lib/deliveredHandler');
+
+let consoleSpy = jest.spyOn(console, 'log');
+
+describe('test the pickupHandler independently', () => {
+  it('pickupHandler works', () => {
+    
+  })
+
+
+})
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
 
 const globalEmitter = require('./lib/events.js');
+const pickupHandler = require('./lib/pickupHandler');
 
+
+globalEmitter.on('orderPlaced', pickupHandler);
+
+require('./lib/order.js');
